94a2d9b93663606ba50d5c17be4aa7b95c661786
Justine <justine.d-andrea@outlook.com>
2026-04-17 21:20:03 +0200

    Structure finale propre

---
+++ src/main/java/com/motogp/projet_gestion/ProjetGestionApplication.java
@@ -0,0 +1,13 @@
+package com.motogp.projet_gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motogp/projet_gestion/ProjetGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motogp.projet_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
